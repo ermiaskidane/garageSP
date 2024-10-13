e96111cde8723f51b0a9ea1c26b231606bfdeef7
ermias kidane <ermiaskidanegebre@gmail.com>
2024-10-13 18:43:59 +0100

    chore: clear some junks

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function RemappingService() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.5453690319307!2d-0.2203214316747949!3d51.48718692801315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760f3ee84ced85%3A0x57831dd7b6c971b9!2s12%20Bramber%20Rd%2C%20London%20W14%209PB%2C%20UK!5e0!3m2!1sen!2sus!4v1697145786750!5m2!1sen!2sus" 
             className="w-full h-full rounded-lg shadow-md"
             style={{border:0}} 
-            allowFullScreen="" 
+            allowFullScreen={true}
             loading="lazy" 
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
